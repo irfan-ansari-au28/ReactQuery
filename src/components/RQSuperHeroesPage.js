45fc1c0475253156e49f88f262eed1571f2a21f0
irfan-ansari-au28 <irfan398649@gmail.com>
2022-03-17 22:04:22 +0530

    Implemented routing

---
+++ src/components/RQSuperHeroesPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const RQSuperHeroesPage = () =>{
+    return <h1>RQSuperHeroesPage</h1>
+}
+
+export default RQSuperHeroesPage
---
+++ src/components/SuperHeroesPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SuperHeroesPage = () =>{
+    return <h1>SuperHeroesPage</h1>
+}
+
+export default SuperHeroesPage
