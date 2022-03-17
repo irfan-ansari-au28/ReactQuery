9a73dffbb81a09366018547bc712fa531e78ead1
irfan-ansari-au28 <irfan398649@gmail.com>
2022-03-17 22:13:31 +0530

    Fetched superheros using axios

--- src/App.js
+++ src/App.js
@@ -6,30 +6,28 @@ import HomePage from "./components/HomePage";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/super-heroes">Traditional Super Heroes</Link>
-              </li>
-              <li>
-                <Link to="/rq-super-heroes">RQ Super Heroes</Link>
-              </li>
-            </ul>
-          </nav>
-          <Routes>
-            <Route path="/super-heroes" element={<SuperHeroesPage />} />
-            <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
-            <Route path="/" element={<HomePage />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/super-heroes">Traditional Super Heroes</Link>
+            </li>
+            <li>
+              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/super-heroes" element={<SuperHeroesPage />} />
+          <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/SuperHeroesPage.js
+++ src/components/SuperHeroesPage.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const SuperHeroesPage = () =>{
-    return <h1>SuperHeroesPage</h1>
-}
+const SuperHeroesPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
 
-export default SuperHeroesPage
+  useEffect(() => {
+    axios.get("http://localhost:4000/superheroes").then((res) => {
+      setData(res.data);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>Super Heroes Page</h2>
+      {data.map((hero) => {
+        return <div key={hero.id}>{hero.name} </div>;
+      })}
+    </>
+  );
+};
+
+export default SuperHeroesPage;
